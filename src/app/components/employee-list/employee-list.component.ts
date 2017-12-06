a93b32779952a60e9bd0684fcae72c2c40298a94
Marcela Malaver <marcela9409@gmail.com>
2017-12-05 19:37:12 -0500

    configuracion lista para ingresar datos(INSERT Ready)

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireList } from 'angularfire2/database'
 import { EmployeeService } from '../../services/employee.service'
+import { Employee } from '../../models/employee'
 
 @Component({
   selector: 'app-employee-list',
@@ -7,10 +9,11 @@ import { EmployeeService } from '../../services/employee.service'
   styles: []
 })
 export class EmployeeListComponent implements OnInit {
-
+  employeelist : AngularFireList<Employee>
   constructor( private employeeService : EmployeeService ) { }
 
   ngOnInit() {
+    this.employeeService.getData();
   }
 
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Employee } from '../models/Employee'
+import { AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+
+import { Employee } from '../models/Employee';
 
 @Injectable()
 export class EmployeeService {
+  employeeList : AngularFireList<any>;
   selectedEmployee : Employee = new Employee();
-  constructor() { }
+  constructor(private firebase : AngularFireDatabase) { }
 
+  getData(){
+    this.employeeList = this.firebase.list('employees');
+    return this.employeeList;
+  }
+  insertEmployee(employee : Employee){
+    this.employeeList.push({
+      name : employee.name,
+      position : employee.position,
+      office : employee.office,
+      salary : employee.salary
+    });
+  }
 }
