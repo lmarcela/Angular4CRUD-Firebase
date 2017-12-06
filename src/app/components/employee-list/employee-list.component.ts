c417784ec3d23733fc18476326333f8685dbcb43
Marcela Malaver <marcela9409@gmail.com>
2017-12-06 10:51:09 -0500

    configuracion lista para listar datos (GET/READ Ready)

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireList } from 'angularfire2/database'
+/*import { AngularFireList } from 'angularfire2/database'*/
 import { EmployeeService } from '../../services/employee.service'
 import { Employee } from '../../models/employee'
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-employee-list',
@@ -9,11 +10,19 @@ import { Employee } from '../../models/employee'
   styles: []
 })
 export class EmployeeListComponent implements OnInit {
-  employeelist : AngularFireList<Employee>
+  employeelist : Employee[];
   constructor( private employeeService : EmployeeService ) { }
 
   ngOnInit() {
-    this.employeeService.getData();
+    var x = this.employeeService.getData();
+    x.snapshotChanges().subscribe(item =>{
+      this.employeelist=[];
+      item.forEach(element=>{
+        var y=element.payload.toJSON();
+        y["$key"]=element.key;
+        this.employeelist.push(y as Employee);
+      })
+    })
   }
 
 }
